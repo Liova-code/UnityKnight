79c411dc6b28b51d58ac1072923e6172321a885c
Liova-code <leon.loukine@gmail.com>
2024-04-09 16:21:37 +0200

    Firstmac
    Heroes first commit

---
+++ Heroes/Assets/Scripts/Animate.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deplacement : MonoBehaviour
+{
+
+    // Variables de vitesse
+    public float vitesse = 2.0f;
+    public float vitesseMarche = 2.0f;
+    public float vitesseCourse = 10.0f;
+    public float vitesseArriere = 1.0f;
+    public float vitesseLaterale = 2.0f;
+
+    // Variables collider et rigidBody
+    public float hauteurSaut = 8.0f;
+    private bool estAuSol = true;
+
+    // Variables pour les animations
+    private bool estEnMouvement = false;
+    private bool estMarcheAvant = false;
+    private bool estMarcheAvantCourse = false;
+    private bool estMarcheArriere = false;
+    private bool estDroite = false;
+    private bool estGauche = false;
+    private bool estEnSaut = false;
+    //declaration
+    private Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        float deplacementHorizontal = Input.GetAxis("Horizontal") * vitesse * Time.deltaTime;
+        float deplacementVertical = Input.GetAxis("Vertical") * vitesse * Time.deltaTime;
+        // pour identifier les animations différentes selon les états move or not
+        estEnMouvement = deplacementHorizontal != 0 || deplacementVertical != 0;
+        if (estEnMouvement == true)
+        {
+            estEnMouvement = true;
+            Debug.Log("je bouge !!!" + estEnMouvement);
+        }
+        else
+        {
+            estEnMouvement = false;
+            animator.SetBool("Walk", false);
+            animator.SetBool("Run", false);
+            animator.SetBool("Back", false);
+            //Debug.Log("je me cure le nez...." + estEnMouvement);
+        }
+        // marche course et marche-arrière
+        if (deplacementVertical > 0 && !Input.GetKey(KeyCode.LeftControl))
+        {
+            estMarcheAvant = true;
+            estMarcheAvantCourse = false;
+            estMarcheArriere = false;
+            deplacementVertical = Input.GetAxis("Vertical") * vitesseMarche * Time.deltaTime;
+            Debug.Log("Déplacement en avant et je marche");
+            animator.SetBool("Run", false);
+            animator.SetBool("Walk", true);
+            
+        }
+        else if (deplacementVertical > 0 && Input.GetKey(KeyCode.LeftControl))
+        {
+            estMarcheAvant = false;
+            estMarcheAvantCourse = true;
+            estMarcheArriere = false;
+            deplacementVertical = Input.GetAxis("Vertical") * vitesseCourse * Time.deltaTime;
+            Debug.Log("Déplacement en avant et je cours !!! yahooooo");
+            animator.SetBool("Run", true);
+            
+        }
+        else if (deplacementVertical < 0)
+        {
+            estMarcheAvant = false;
+            estMarcheAvantCourse = false;
+            estMarcheArriere = true;
+            deplacementVertical = Input.GetAxis("Vertical") * vitesseArriere * Time.deltaTime;
+            Debug.Log("Déplacement en arrière");
+            animator.SetBool("Back", true);
+        }
+        //gauche droite...
+        if (deplacementHorizontal > 0)
+        {
+            if (estMarcheAvant)
+            {
+                vitesseLaterale = vitesseMarche;
+            }
+            else if (estMarcheAvantCourse)
+            {
+                vitesseLaterale = vitesseCourse;
+            }
+            else if (estMarcheArriere)
+            {
+                vitesseLaterale = vitesseArriere;
+            }
+            deplacementHorizontal = Input.GetAxis("Horizontal") * vitesseLaterale * Time.deltaTime;
+            Debug.Log("Déplacement à droite" + vitesseLaterale);
+        }
+        else if (deplacementHorizontal < 0)
+        {
+            if (estMarcheAvant)
+            {
+                vitesseLaterale = vitesseMarche;
+            }
+            else if (estMarcheAvantCourse)
+            {
+                vitesseLaterale = vitesseCourse;
+            }
+            else if (estMarcheArriere)
+            {
+                vitesseLaterale = vitesseArriere;
+            }
+            deplacementHorizontal = Input.GetAxis("Horizontal") * vitesseLaterale * Time.deltaTime;
+            Debug.Log("Déplacement à gauche" + vitesseLaterale);
+        }
+        // jump necessite collider et rigid body (avec contrainte sur le rigidbody freeze rotation X et Z pour eviter de basculer)
+        if (Input.GetButtonDown("Jump") && estAuSol)
+        {
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, hauteurSaut, 0), ForceMode.Impulse);
+            estAuSol = false;
+            estEnSaut = true;
+            Debug.Log("Je saute !!! youpi");
+        }
+
+        transform.Translate(deplacementHorizontal, 0, deplacementVertical);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Sol"))
+        {
+            estAuSol = true;
+            estEnSaut = false;
+
+        }
+    }
+}
